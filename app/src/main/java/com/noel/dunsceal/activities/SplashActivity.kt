1cd8efe4e0b085aaf164be68eb55cc353126a890
N <noel.mcloughlin@gmail.com>
2019-11-02 20:57:51 +0000

    basic splash screen

---
+++ app/src/main/java/com/noel/dunsceal/activities/SplashActivity.kt
@@ -0,0 +1,39 @@
+package com.noel.dunsceal.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import com.noel.dunsceal.R
+import com.noel.dunsceal.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        makeFullScreen()
+        setContentView(R.layout.splash_activity)
+// Using a handler to delay loading the MainActivity
+        Handler().postDelayed({
+            // Start activity
+            startActivity(Intent(this, MainActivity::class.java))
+            // Animate the loading of new activity
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            // Close this activity
+            finish()
+        }, 2000)
+    }
+
+    private fun makeFullScreen() {
+        // Remove Title
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        // Make Fullscreen
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        // Hide the toolbar
+        supportActionBar?.hide()
+    }
+}
